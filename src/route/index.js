391f4db90ae97cbbeb39c0d52c980c1276b36748
chip22294 <chip22294@gmail.com>
2023-06-12 19:04:07 +0300

    Fixed mistakes

--- src/route/index.js
+++ src/route/index.js
@@ -32,16 +32,13 @@ var footer = {
 }
 // ================================================================
 
-// router.get Створює нам один ентпоїнт
+// //           ↙ тут вводимо шлях (PATH) до сторінки
+// router.get('/', function (req, res) {
 
-//           ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/', function (req, res) {
-  // res.render генерує нам HTML сторінку
-
-  //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {})
-  //                  ↑↑ сюди вводимо JSON дані
-})
+//   //            ↙ cюди вводимо назву файлу з сontainer
+//   res.render('index', {})
+//   //                  ↑↑ сюди вводимо JSON дані
+// })
 
 // ================================================================
 
@@ -3274,8 +3271,8 @@ router.get('/shopcatalog', function (req, res) {
   //                  ↑↑ сюди вводимо JSON дані
 })
 
-router.get('/index8', function (req, res) {
-  res.render('index8', {
+router.get('/', function (req, res) {
+  res.render('index', {
     layout: 'index',
     page: {
       title: { name: 'Resume project' },
